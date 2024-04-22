a717c1b68332a18f0b81729faa74ff8b49fe62f3
Malik59 <mal.k@laposte.net>
2024-04-22 16:11:19 +0200

    liaison entre le formulaire et la BDD

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -37,6 +38,9 @@ class RegisterType extends AbstractType
                 'attr' => [
                     'placeholder' => "Veuillez entrer votre nom"
                 ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Valider'
             ])
         ;
     }
